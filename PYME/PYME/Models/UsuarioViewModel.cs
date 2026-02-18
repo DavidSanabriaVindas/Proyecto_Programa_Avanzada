2f3cf092a54f8c4c633edc14f6584289a76f86bc
Santiago Orozco <sorozco80826@ufide.ac.cr>
2026-02-18 00:34:24 -0600

    Vistas: Index y Crear de Usuarios

---
+++ PYME/PYME/Models/UsuarioViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PYME.Models
+{
+    public class UsuarioViewModel
+    {
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El primer apellido es obligatorio")]
+        public string Primer_Apellido { get; set; }
+
+        [Required(ErrorMessage = "El segundo apellido es obligatorio")]
+        public string Segundo_Apellido { get; set; }
+
+        [Required(ErrorMessage = "El username es obligatorio")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "La contraseña es obligatoria")]
+        public string Password { get; set; }
+
+        public string? Direccion_Exacta { get; set; }
+
+        public int? Telefono { get; set; }
+
+        [EmailAddress(ErrorMessage = "El formato del correo no es válido")]
+        public string? Correo { get; set; }
+
+        [Required(ErrorMessage = "Debe seleccionar un rol")]
+        public int Id_Rol { get; set; }
+    }
+}
