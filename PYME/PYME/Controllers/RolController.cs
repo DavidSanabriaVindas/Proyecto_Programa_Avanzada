8828d175384799c30159eda99b6e8a135a084b10
josefgonzalezz <jgonzalez50082@ufide.ac.cr>
2026-02-18 00:19:51 -0600

    Controller of user and rol and view

---
+++ PYME/PYME/Controllers/RolController.cs
@@ -0,0 +1,84 @@
+﻿using PYME.Models;
+using PYME.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PYME.Controllers
+{
+    [Route("rol")]
+    public class RolController : Controller
+    {
+        private readonly IRolService _rolService;
+
+        public RolController(IRolService rolService)
+        {
+            _rolService = rolService;
+        }
+
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            var roles = _rolService.ObtenerTodos();
+            return View(roles);
+        }
+
+        [HttpGet("detalle/{id:int}")]
+        public IActionResult Detalle(int id)
+        {
+            var rol = _rolService.ObtenerPorId(id);
+
+            if (rol == null)
+                return NotFound();
+
+            return View(rol);
+        }
+
+        [HttpGet("crear")]
+        public IActionResult Crear()
+        {
+            return View(new Rol());
+        }
+
+        [HttpPost("crear")]
+        public IActionResult Crear(Rol rol)
+        {
+            if (!ModelState.IsValid)
+                return View(rol);
+
+            _rolService.CrearRol(rol);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet("editar/{id:int}")]
+        public IActionResult Editar(int id)
+        {
+            var rol = _rolService.ObtenerPorId(id);
+
+            if (rol == null)
+                return NotFound();
+
+            return View(rol);
+        }
+
+        [HttpPost("editar/{id:int}")]
+        public IActionResult Editar(int id, Rol rol)
+        {
+            if (id != rol.Id_Rol)
+                return BadRequest();
+
+            if (!ModelState.IsValid)
+                return View(rol);
+
+            _rolService.ActualizarRol(rol);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost("eliminar/{id:int}")]
+        public IActionResult Eliminar(int id)
+        {
+            _rolService.EliminarRol(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ PYME/PYME/Controllers/UsuarioController.cs
@@ -0,0 +1,106 @@
+﻿using PYME.Models;
+using PYME.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PYME.Controllers
+{
+    [Route("usuario")]
+    public class UsuarioController : Controller
+    {
+        private readonly IUsuariosService _usuarioService;
+        private readonly IRolService _rolService;
+
+        public UsuarioController(
+            IUsuariosService usuarioService,
+            IRolService rolService)
+        {
+            _usuarioService = usuarioService;
+            _rolService = rolService;
+        }
+
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            var usuarios = _usuarioService.ObtenerTodos();
+            return View(usuarios);
+        }
+
+        [HttpGet("detalle/{id:int}")]
+        public IActionResult Detalle(int id)
+        {
+            var usuario = _usuarioService.ObtenerDetalle(id);
+
+            if (usuario == null)
+                return NotFound();
+
+            return View(usuario);
+        }
+
+        [HttpGet("crear")]
+        public IActionResult Crear()
+        {
+            ViewBag.Roles = _rolService.ObtenerActivos();
+            return View(new Usuario());
+        }
+
+        [HttpPost("crear")]
+        public IActionResult Crear(Usuario usuario)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Roles = _rolService.ObtenerActivos();
+                return View(usuario);
+            }
+
+            if (!_usuarioService.CrearUsuario(usuario))
+            {
+                ModelState.AddModelError("Username", "Ya existe un usuario con este username");
+                ViewBag.Roles = _rolService.ObtenerActivos();
+                return View(usuario);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet("editar/{id:int}")]
+        public IActionResult Editar(int id)
+        {
+            var usuario = _usuarioService.ObtenerDetalle(id);
+
+            if (usuario == null)
+                return NotFound();
+
+            ViewBag.Roles = _rolService.ObtenerActivos();
+            return View(usuario);
+        }
+
+        [HttpPost("editar/{id:int}")]
+        public IActionResult Editar(int id, Usuario usuario)
+        {
+            if (id != usuario.Id_Usuario)
+                return BadRequest();
+
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Roles = _rolService.ObtenerActivos();
+                return View(usuario);
+            }
+
+            if (!_usuarioService.ActualizarUsuario(usuario))
+            {
+                ModelState.AddModelError("Username", "Ya existe un usuario con este username");
+                ViewBag.Roles = _rolService.ObtenerActivos();
+                return View(usuario);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost("eliminar/{id:int}")]
+        public IActionResult Eliminar(int id)
+        {
+            _usuarioService.EliminarUsuario(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
