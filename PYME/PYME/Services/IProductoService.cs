8b2f5ce22d64b5d773b843196093131efb5da187
DavidG <davidgonzalezlda497@gmail.com>
2026-03-17 20:38:16 -0600

    Service Creation

---
+++ PYME/PYME/Services/IProductoService.cs
@@ -0,0 +1,14 @@
+﻿using PYME.Models;
+
+namespace PYME.Services
+{
+    public interface IProductoService
+    {
+        List<Producto> ObtenerTodos();
+        Producto? ObtenerDetalle(int id);
+
+        bool CrearProducto(ProductoViewModel modelo);
+        bool ActualizarProducto(int id, ProductoViewModel modelo);
+        bool EliminarProducto(int id);
+    }
+}
