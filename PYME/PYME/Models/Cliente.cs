880f6538778eb33f96202fa28e62092453b4ebdf
Santiago Orozco <sorozco80826@ufide.ac.cr>
2026-03-24 22:42:47 -0600

    Creation of the client model and repository
    Creation of the client model and repository.

---
+++ PYME/PYME/Models/Cliente.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PYME.Models
+{
+    public class Cliente
+    {
+        [Key]
+        public int Id_Cliente { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El primer apellido es obligatorio.")]
+        public string Primer_Apellido { get; set; }
+
+        [Required(ErrorMessage = "El segundo apellido es obligatorio.")]
+        public string Segundo_Apellido { get; set; }
+
+        public int? Telefono { get; set; }
+
+        [EmailAddress(ErrorMessage = "El formato del correo no es válido.")]
+        public string? Correo { get; set; }
+
+        public string? Direccion_Exacta { get; set; }
+    }
+}
---
+++ PYME/PYME/Repositories/IClienteRepository.cs
@@ -0,0 +1,13 @@
+﻿using PYME.Models;
+
+namespace PYME.Repositories
+{
+    public interface IClienteRepository
+    {
+        List<Cliente> ObtenerTodos();
+        Cliente? ObtenerPorId(int id);
+        void Agregar(Cliente cliente);
+        void Actualizar(Cliente cliente);
+        void Eliminar(int id);
+    }
+}
