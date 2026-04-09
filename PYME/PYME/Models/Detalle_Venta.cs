829462094b0baefb9b4587b86dc1cd21b250a404
josefgonzalezz <jgonzalez50082@ufide.ac.cr>
2026-04-08 22:16:10 -0600

    Sell
    Model of sell and sell details

---
+++ PYME/PYME/Models/Detalle_Venta.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace PYME.Models
+{
+    public class Detalle_Venta
+    {
+        [Key]
+        public int Id_Detalle_Venta { get; set; }
+
+        [Required(ErrorMessage = "La cantidad es obligatoria.")]
+        public int Cantidad { get; set; }
+
+        [Required(ErrorMessage = "El precio unitario es obligatorio.")]
+        public int Precio_Unitario { get; set; }
+
+        public int Subtotal { get; set; }
+
+        [ForeignKey(nameof(Venta))]
+        public int Id_Venta { get; set; }
+        [ValidateNever]
+        public Venta? Venta { get; set; }
+
+        [ForeignKey(nameof(Producto))]
+        public int Id_Producto { get; set; }
+        [ValidateNever]
+        public Producto? Producto { get; set; }
+    }
+}
---
+++ PYME/PYME/Models/Venta.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace PYME.Models
+{
+    public class Venta
+    {
+        [Key]
+        public int Id_Venta { get; set; }
+
+        public DateTime? Fecha_Venta { get; set; } = DateTime.Now;
+
+        public int Total { get; set; }
+
+        public string? Observaciones { get; set; }
+
+        [Required(ErrorMessage = "El estado es obligatorio.")]
+        public string Estado { get; set; } = "Pendiente";
+
+        [ForeignKey(nameof(Cliente))]
+        public int Id_Cliente { get; set; }
+        [ValidateNever]
+        public Cliente? Cliente { get; set; }
+
+        [ForeignKey(nameof(Usuario))]
+        public int Id_Usuario { get; set; }
+        [ValidateNever]
+        public Usuario? Usuario { get; set; }
+
+        public List<Detalle_Venta> Detalles { get; set; } = new();
+    }
+}
